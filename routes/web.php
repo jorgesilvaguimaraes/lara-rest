13ecb5fb18c9eb96d0e5abc6b6c3ec1ea4f2456f
Jorge Guimaraes <jorginhoad@gmail.com>
2019-01-21 17:10:54 -0300

    alteracao do arquivo de rota web e de configuracao do banco de dados

--- routes/web.php
+++ routes/web.php
@@ -10,23 +10,23 @@
 //| contains the "web" middleware group. Now create something great!
 //|
 //*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+//
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+//
+//Auth::routes();
+//
+//Route::get('/home', 'HomeController@index')->name('home');
+//
+//Auth::routes();
+//
+//Route::get('/home', 'HomeController@index')->name('home');
+//
+//Auth::routes();
+//
+//Route::get('/home', 'HomeController@index')->name('home');
+//
+//Auth::routes();
+//
+//Route::get('/home', 'HomeController@index')->name('home');
