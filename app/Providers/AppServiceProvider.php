a029530cd45245a903f3510f07e4a12f9ba001e2
Jorge Guimaraes <jorginhoad@gmail.com>
2019-02-20 14:17:30 -0300

    criando o repository Categorias

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        ////
+        array_map(function ($value) {
+            if (preg_match("/(\\w+)(Eloquent).php/", $value, $newValue))
+            {
+                $this->app->bind(
+                    "App\\Repositories\\{$newValue[1]}",
+                    "App\\Repositories\\{$newValue[1]}Eloquent"
+                );
+            }
+        }, scandir(app_path('Repositories')));
     }
 
     /**
